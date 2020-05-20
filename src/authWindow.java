3cf4f5a5983dde9161ee8e6090b20c4f2c76b22a
subbu-123 <subhankarsahoo94@gmail.com>
2020-05-20 16:01:33 +0530

    first commit

---
+++ src/authWindow.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class authWindow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\subha\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		// code to handle windows pop up
+		driver.get("https://admin:admin@the-internet.herokuapp.com/");
+		driver.findElement(By.xpath("//*[text() = 'Basic Auth']")).click();
+	}
+
+}
